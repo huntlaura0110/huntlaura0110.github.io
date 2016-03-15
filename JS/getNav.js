c98124350264d526a4b11caad4af5ed68b71d5af
huntlaura0110 <huntlaura0110@gmail.com>
2016-03-15 08:55:16 -0400

    GetNav connection

---
+++ JS/getNav.js
@@ -0,0 +1,7 @@
+//get the file (replace with your own url)
+$.get('http://huntlaura0110.github.io/Partials/nav.html'), function(data){
+$(document).ready(function(){
+$('.container').prepend(data);
+
+});
+};
