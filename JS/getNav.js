cc415fd2d5806c04f8bbee82dafee5875ab46c96
huntlaura0110 <huntlaura0110@gmail.com>
2016-03-15 10:38:22 -0400

    footer

--- JS/getNav.js
+++ JS/getNav.js
@@ -4,8 +4,17 @@ $.get('http://huntlaura0110.github.io/Partials/nav.html', function(data){
 $(document).ready(function(){
 
 $('.container').prepend(data);
+
+})
+})
+
+
+$.get('http://huntlaura0110.github.io/Partials/nav.html', function(foot){
+
+$(document).ready(function(){
+
+$('.container').prepend(foot);
 $('.container').fadeIn('slow');
 
 })
-
 })
