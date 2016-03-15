bba391cd6f91a80e5b881ccfbbdac1879d956126
huntlaura0110 <huntlaura0110@gmail.com>
2016-03-15 08:56:04 -0400

    added spacing

--- JS/getNav.js
+++ JS/getNav.js
@@ -1,6 +1,8 @@
 //get the file (replace with your own url)
 $.get('http://huntlaura0110.github.io/Partials/nav.html'), function(data){
+
 $(document).ready(function(){
+  
 $('.container').prepend(data);
 
 });
