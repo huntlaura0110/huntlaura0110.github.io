5c083352e5fa4a8ab079547ddbf371f4dc88661b
huntlaura0110 <huntlaura0110@gmail.com>
2016-03-15 09:08:33 -0400

    bracket fixed

--- JS/getNav.js
+++ JS/getNav.js
@@ -1,5 +1,5 @@
 //get the file (replace with your own url)
-$.get'http://huntlaura0110.github.io/Partials/nav.html', function(data){
+$.get 'http://huntlaura0110.github.io/Partials/nav.html', function(data){
 
 $(document).ready(function(){
 
@@ -7,4 +7,4 @@ $('.container').prepend(data);
 
 })
 
-}
+})
