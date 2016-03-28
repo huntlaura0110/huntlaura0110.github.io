295cea3c583e678f8d92b3e6a65268d9812e4b23
huntlaura0110 <huntlaura0110@gmail.com>
2016-03-28 14:28:25 -0400

    footer html

---
+++ otherprojects/finalWebsite/js/getNav.js
@@ -0,0 +1,20 @@
+//get the file (replace with your own url)
+$.get('http://huntlaura0110.github.io/otherprojects/finalWebsite/partials/nav.html', function(data){
+
+$(document).ready(function(){
+
+$('.container').prepend(data);
+
+})
+})
+
+
+$.get('http://huntlaura0110.github.io/Partials/footer.html', function(foot){
+
+$(document).ready(function(){
+
+$('.container').prepend(foot);
+$('.container').fadeIn('slow');
+
+})
+})
