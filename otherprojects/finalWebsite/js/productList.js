333af75b50a1a796f48166db32cf7ac679b0a80b
huntlaura0110 <huntlaura0110@gmail.com>
2016-03-29 10:36:04 -0400

    dfJVNdifbjnBDFJNdo[jfbvn[v

--- otherprojects/finalWebsite/js/productList.js
+++ otherprojects/finalWebsite/js/productList.js
@@ -1,37 +1,5 @@
 $(document).ready(function(){
 
-$('.basic-info').hide();
-$('.extended-info').hide();
-$('.basic-info h3').hide();
-
-$('#getInfo').on('click', function() {
-
-var url = "http://huntlaura0110.github.io/otherprojects/finalWebsite/jsonDatabase/finalWebsite.json";
-
-  $.getJSON(url, function (data){
-
-var html = "<table class= 'table table-hover table-striped'>" +
-"<tr><th>Name</th><th>Seasons</th><th>Description</th></tr>";
-
-    $.each(data, function(index, item){
-
-    html += "<tr>" +
-    "<td>"+item.name+"</td>" +
-    "<td>"+item.seasons+"</td>" +
-    "<td>"+item.description+"</td>" +
-    "</tr>";
-  }) //each
-
-    html += "</table>";
-
-    $('.extended-info').append(html);
-
-
-
-
-  }) //getJSON
-
-}) //click
 
 
 }) //end
