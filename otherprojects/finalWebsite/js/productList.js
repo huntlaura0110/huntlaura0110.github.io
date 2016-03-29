f7475a8c8981c5e3783cf50bb623bbe97feb9f62
huntlaura0110 <huntlaura0110@gmail.com>
2016-03-29 12:50:13 -0400

    genre added

--- otherprojects/finalWebsite/js/productList.js
+++ otherprojects/finalWebsite/js/productList.js
@@ -8,12 +8,13 @@ var url = "http://huntlaura0110.github.io/otherprojects/finalWebsite/jsonDatabas
   $.getJSON(url, function (data){
 
 var html = "<table class= 'table table-hover table-striped'>" +
-"<tr><th>Name</th><th>Release Date</th><th>Seasons</th><th>Description</th><th>Rated</th></tr>";
+"<tr><th>Name</th><th>Genre</th><th>Release Date</th><th>Seasons</th><th>Description</th><th>Rated</th></tr>";
 
     $.each(data, function(index, item){
 
     html += "<tr>" +
     "<td>"+item.name+"</td>" +
+    "<td>"+item.genre+"</td>" +
     "<td>"+item.release+"</td>" +
     "<td>"+item.seasons+"</td>" +
     "<td>"+item.description+"</td>" +
