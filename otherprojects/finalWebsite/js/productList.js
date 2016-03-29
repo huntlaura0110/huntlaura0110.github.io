16c169c8c6a3c530317b72cca6238b18032f8628
huntlaura0110 <huntlaura0110@gmail.com>
2016-03-29 11:58:15 -0400

    button

--- otherprojects/finalWebsite/js/productList.js
+++ otherprojects/finalWebsite/js/productList.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
 alert('something');
 
+$("#getInfo").on("click", function() {
+
+var url = "http://huntlaura0110.github.io/otherprojects/finalWebsite/jsonDatabase/finalWebsite.json";
+
+  $.getJSON(url, function (data){
+
+var html = "<table class= 'table table-hover table-striped'>" +
+"<tr><th>Name</th><th>Seasons</th><th>Description</th></tr>";
+
+    $.each(data, function(index, item){
+
+    html += "<tr>" +
+    "<td>"+item.name+"</td>" +
+    "<td>"+item.seasons+"</td>" +
+    "<td>"+item.description+"</td>" +
+    "</tr>";
+  }) //each
+
+    html += "</table>";
+
+    $("#data").append(html);
+
+
+
+  }) //getJSON
+
+}) //Click
 
 }); //end
