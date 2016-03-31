ad0e7ecf9e2b693ffde15e593155f47eef9c851b
huntlaura0110 <huntlaura0110@gmail.com>
2016-03-31 18:12:11 -0400

    merged js files

--- otherprojects/finalWebsite/js/finalWebsite.js
+++ otherprojects/finalWebsite/js/finalWebsite.js
@@ -36,4 +36,34 @@ $("#myButton").on("mouseenter", function() {
   $("#log").append("<br>Value of checkbox is: " + myCheckValues);
 })
 
+//Get table of all info when click the button
+$("#getInfo").on("click", function() {
+
+var url = "http://huntlaura0110.github.io/otherprojects/finalWebsite/jsonDatabase/finalWebsite.json";
+
+  $.getJSON(url, function (data){
+
+var html = "<table class= 'table table-hover table-striped'>" +
+"<tr><th>Name</th><th>Genre</th><th>Release Date</th><th>Seasons</th><th>Description</th><th>Rated</th></tr>";
+
+    $.each(data, function(index, item){
+
+    html += "<tr>" +
+    "<td>"+item.name+"</td>" +
+    "<td>"+item.genre+"</td>" +
+    "<td>"+item.release+"</td>" +
+    "<td>"+item.seasons+"</td>" +
+    "<td>"+item.description+"</td>" +
+    "<td>"+item.rated+"</td>" +
+    "</tr>";
+  }) //each
+
+    html += "</table>";
+
+    $("#data").append(html);
+
+  }) //getJSON
+
+}) //Click
+
 })
