7171c1a6068eb4bbee1727196f756b67a009903f
huntlaura0110 <huntlaura0110@gmail.com>
2016-04-03 23:24:18 -0400

    lab 9

--- otherprojects/ajaxExamples/js/lab9.js
+++ otherprojects/ajaxExamples/js/lab9.js
@@ -1,35 +1,29 @@
 $(document).ready(function(){
 
-$('#getOther').click(function(){
-  $.getJSON("../jsonDatabase/lab9.json", function(data) {
+  $("#getOther").on("click",function(){
 
-              console.dir(data);
-              var html = "";
+    $.getJSON("http://huntlaura0110.github.io/otherprojects/ajaxExamples/jsonDatabase/lab9.json", function(data) {
 
-              $.each(data, function(index, item) {
-                  html += '<div class="col-md-4">' +
-                    '<div class="Name">' + item.name + '</div>' +
-                    '<div class="email"><small>email </small>' + item.email + '</div>' +
-                    '<div class="gender"><small>gender</small>' + item.gender + '</div>' +
-                    '<div class="phone"><small> phone </small>' + item.phone + '</div>' +
-                    //deleted commentsContainer
-                    '<div class="panel panel-default">' + //added
-                    '<div class="panel-heading">Renter Comments</div>'; //added
-                  $.each(item.comments, function(ind, i) {
-                      html += '<div class="panel-body">' + //added
-                        '<div class="renterName">' + i.username + '</div>' +
-                        '<div class="renterComment">' + i.comment + '</div>' +
-                        '<div class="renterStars">';
+                console.dir(data);
 
+                  var html="<table class='table table-hover table-striped'>"+
+                "<tr><th>Name</th><th>age</th><th>Company</th></tr>";
+                  $.each(data, function(index, item){
+                  //  $("#data").append(item.name);
+                html+="<tr>"+
+                "<td>" +item.name +"</td>"+
+                "<td>"+item.phone + "</td>"+
+                "<td>"+item.email + "</td>"+
+                "<td>"+item.address + "</td>"+
 
+                "</tr>";
+                  }) // each end
+                      html+="</table>";
+                      $("#data").append(html);
+                    html += '</div>' + //panel
+                      '</div>'; //col-md-4
+                  }) //each person
 
-                        '</div>'; //panel body
-                    }) //each comment
+}) //click end
 
-                  html += '</div>' + //panel
-                    '</div>'; //col-md-4
-                }) //each cat
-
-          }); //click
-
-}); // document ready
+}) // doc end
